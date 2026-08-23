f49e9d30d921c114b29181831d976d1b46ae5cc9
palaknandanwar-lab <palaknandanwar@gmail.com>
2026-08-23 20:10:31 +0530

    Clean up whitespace and formatting in app.py

--- backend/app.py
+++ backend/app.py
@@ -10,16 +10,11 @@ blinker.Namespace = DummyNamespace
 
 from flask import Flask
 from flask_cors import CORS
-
 from routes import routes
 
-
 app = Flask(__name__)
-
 CORS(app)
-
 app.register_blueprint(routes)
-
 
 if __name__ == "__main__":
     app.run(
